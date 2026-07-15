fe6abdb5b54a3b74a3716bef549f4ebf13e41d58
bagasadit123 <bagasadit430@gmail.com>
2026-07-15 10:51:43 +0700

    GET

--- index.js
+++ index.js
@@ -16,3 +16,28 @@ const pool = new Pool({
     password: '123456789',
     port: 5432,
 });
+
+app.get('/biodata', (req, res) => {
+    pool.query('SELECT * FROM biodata ORDER BY id ASC')
+        .then(result => {
+            res.json(result.rows);
+        })
+        .catch(err => {
+            console.error('Error executing query:', err.stack);
+            res.status(500).json({ error: 'Database error' });
+        });
+});
+app.get('/biodata/:id', (req, res) => {
+    const { id } = req.params;
+    pool.query('SELECT * FROM biodata WHERE id = $1', [id])
+        .then(result => {
+            if (result.rows.length === 0) {
+                return res.status(404).json({ message: 'Data tidak ditemukan' });
+            }
+            res.json(result.rows[0]);
+        })
+        .catch(err => {
+            console.error('Error executing query:', err.stack);
+            res.status(500).json({ error: 'Database error' });
+        });
+});
